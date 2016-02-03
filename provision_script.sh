4b181bf9a9cd2a344bd8c14279e2f16c06589380
Chaudhry Junaid Anwar <junaid.anwar@vizteck.com>
2016-02-03 18:40:05 +0500

    additions to provision script

--- provision_script.sh
+++ provision_script.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get install -y  vim git g++ mysql-client
 wget https://nodejs.org/dist/v4.2.4/node-v4.2.4-linux-x64.tar.gz
 sudo tar -C /usr/local --strip-components 1 -xzf node-v4.2.4-linux-x64.tar.gz
 sudo npm install -g bower mocha grunt-cli shipit-cli pm2
+pm2 completion install
